7dc6282e09714e60a254728537da932615c7d36a
arbabf <arbab.fahm@gmail.com>
2024-11-08 04:57:51 +1100

    feat: play audio on button click

---
+++ Assets/SafespacesAssets/Scripts/MenuActions/AmbientSoundManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmbientSoundManager : MonoBehaviour
+{
+    public Button[] soundButtons;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        soundButtons[0].onClick.AddListener(delegate { PlayAudioSource("e"); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // TODO: we already have all our AudioSources that exist and all that; we do not need to find an AudioSource by name.
+    // The better way would be to link each AudioSource child in our prefab to an element in an array, and link our buttons
+    // to that array.
+    // For now, as a PoC, this is fine.
+    void PlayAudioSource(string sourceName)
+    {
+        AudioSource source = transform.Find("AmbientFan").GetComponent<AudioSource>();
+        source.Play();
+    }
+
+    void StopAudioSource(string sourceName)
+    {
+
+    }
+}
