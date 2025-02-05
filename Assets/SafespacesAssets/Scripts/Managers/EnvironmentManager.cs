b7bc375ddc199d6c580ff7982fb63e131ff7c3bb
arbabf <arbab.fahm@gmail.com>
2025-02-06 02:57:27 +1100

    feat: moveable furniture system

--- Assets/SafespacesAssets/Scripts/Managers/EnvironmentManager.cs
+++ Assets/SafespacesAssets/Scripts/Managers/EnvironmentManager.cs
@@ -1,6 +1,10 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class EnvironmentManager : MonoBehaviour
@@ -12,6 +16,7 @@ public class EnvironmentManager : MonoBehaviour
 
     private InputAction moveAction;
     private bool roomActive;
+    private GameObject attachedObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +30,23 @@ public class EnvironmentManager : MonoBehaviour
         moveAction.performed += MoveObject;
     }
 
+    void Update()
+    {
+        if (attachedObject)
+        {
+            interactor.TryGetCurrent3DRaycastHit(out RaycastHit raycast);
+
+            // did this hit the floor?
+            if (raycast.normal == Vector3.up)
+            {
+                attachedObject.transform.position = raycast.point;
+                Vector3 angles = attachedObject.transform.eulerAngles;
+                angles.y = interactor.rayOriginTransform.eulerAngles.y;
+                attachedObject.transform.eulerAngles = angles;
+            }
+        }
+    }
+
     public void ToggleEnvironment()
     {
         roomActive = !roomActive;
@@ -34,6 +56,102 @@ public class EnvironmentManager : MonoBehaviour
 
     public void MoveObject(InputAction.CallbackContext context)
     {
+        if (attachedObject)
+        {
+            DetachObject();
+        }
+        else
+        {
+            interactor.TryGetCurrent3DRaycastHit(out RaycastHit raycast);
+            XRSimpleInteractable obj = raycast.transform.root.gameObject.GetComponent<XRSimpleInteractable>();
+            if (obj && (obj.interactionLayers.value & 1 << 2) > 0)
+            {
+                AttachObject(obj);
+            }
+        }
+    }
 
+    public void AttachObject(XRSimpleInteractable interactable)
+    {
+        attachedObject = interactable.gameObject;
+
+        foreach (Renderer renderer in attachedObject.gameObject.GetComponentsInChildren<Renderer>())
+        {
+            if (renderer)
+            {
+                foreach (Material mat in renderer.materials)
+                {
+                    if (mat.HasColor("_BaseColor"))
+                    {
+                        // make this object transparent
+                        mat.SetFloat("_Surface", 1);
+
+                        Color c = mat.GetColor("_BaseColor");
+                        c.a = 0.5f;
+                        mat.SetColor("_BaseColor", c);
+
+                        mat.SetFloat("_Blend", 0.0f);
+                        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+
+                        mat.SetInt("_ZWrite", 0);
+                        mat.DisableKeyword("_ALPHATEST_ON");
+                        mat.EnableKeyword("_ALPHABLEND_ON");
+                        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        mat.renderQueue = 3000;
+                    }
+                }
+            }
+        }
+
+        // disable ray interactor raycasts hitting this object
+        int layer = LayerMask.NameToLayer("Ignore Raycast");
+        attachedObject.layer = layer;
+        foreach (Transform child in attachedObject.transform)
+        {
+            child.gameObject.layer = layer;
+        }
+    }
+
+    public void DetachObject()
+    {
+        foreach (Renderer renderer in attachedObject.gameObject.GetComponentsInChildren<Renderer>())
+        {
+            if (renderer)
+            {
+                foreach (Material mat in renderer.materials)
+                {
+                    if (mat.HasColor("_BaseColor"))
+                    {
+                        // make this object opaque
+                        mat.SetFloat("_Surface", 0.0f);
+
+                        Color c = mat.GetColor("_BaseColor");
+                        c.a = 1f;
+                        mat.SetColor("_BaseColor", c);
+
+                        mat.SetFloat("_Blend", 0.0f);
+                        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+
+                        mat.SetInt("_ZWrite", 1);
+                        mat.DisableKeyword("_ALPHATEST_ON");
+                        mat.DisableKeyword("_ALPHABLEND_ON");
+                        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        mat.renderQueue = -1;
+                    }
+                }
+            }
+        }
+
+        // re-enable ray interactor raycasts hitting this object
+        int layer = LayerMask.NameToLayer("Default");
+        attachedObject.layer = layer;
+        foreach (Transform child in attachedObject.transform)
+        {
+            child.gameObject.layer = layer;
+        }
+
+        attachedObject = null;
     }
 }
